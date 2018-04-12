c6bfaf6fa0c178e82d17bd41c63cb5cfefa56764
Sarin Iamsangtham <Iamsangsam@gmail.com>
2018-04-12 13:53:16 -0400

    Bounce Changes
    Fixed bouncing of the aim laser and the cannonball.
    
    Former now actually bounces once
    
    Latter now has normal behavior at shallow angles

---
+++ BattleShips/Assets/Scripts/AimLineBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimLineBehavior : MonoBehaviour {
+
+    private LineRenderer lr;
+    private float reflectDistance;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 3;
+        reflectDistance = 2f;
+    }
+
+    private void Update()
+    {
+        lr.SetPosition(0, transform.position);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        Ray b;
+        RaycastHit hit, hit2;
+
+
+        if (Physics.Raycast(ray, out hit)){
+            if (hit.collider)
+            {
+
+                Vector3 reflectAngle = Vector3.Reflect(ray.direction, hit.normal);
+
+                //Make a second ray starting at the hit point and going outward in the normal direction
+                b = new Ray (hit.point, reflectAngle);
+                Physics.Raycast(b, out hit2);
+
+                //lr.SetPosition(1, hit.point);
+                lr.SetPositions(new[] { transform.position, hit.point,  hit2.point});
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, transform.forward * 1000);
+        }
+
+    }
+}   
---
+++ BattleShips/Assets/Scripts/CannonballBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballBehavior : MonoBehaviour
+{
+
+    public LayerMask collisionMask;
+
+    public float speed;
+    public AudioClip bounceSound;
+    public AudioSource bounceSource;
+
+
+    private Vector3 dir, reflectDir;
+    private int bouncesToLive;
+    private float basePitch;
+
+    Ray ray;
+    RaycastHit hit;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        dir = Vector3.forward;
+        bouncesToLive = 5;
+        speed = 750;
+        basePitch = bounceSource.pitch;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.Translate(dir * speed * Time.deltaTime);
+       
+        //keep ray from the cannonball's position
+        ray = new Ray(transform.position, transform.forward);
+       
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //only cast the data out to hit on collision against a wall
+        Physics.Raycast(ray, out hit);
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+            bounceSource.pitch -= 0.1f;
+            bounceSource.Play();
+            bouncesToLive--;
+        }
+        //If there are still bounces it can make, adjust the bounce angle of the cannonball and also decrease the pitch of the bounce noise as indication
+
+        if (bouncesToLive <= 0)
+        {
+            bounceSource.Play();
+            Destroy(this.gameObject);
+            GameObject.FindGameObjectWithTag("Manager").GetComponent<ControlSwitcher>().timeLeft = 0;
+            GameObject.FindGameObjectWithTag("Manager").GetComponent<ControlSwitcher>().playerFired = false;
+            bounceSource.pitch = basePitch;
+        }
+
+
+    }
+}
