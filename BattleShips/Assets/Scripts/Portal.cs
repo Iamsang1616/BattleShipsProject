23e064e30845b2a365daf0b0cd049802a7b374fd
sparK1N9 <spacecqcenhancer@gmail.com>
2018-04-21 19:28:50 -0400

    new scene. new feature in the scene: portals for cannon balls. black in, white out.

---
+++ BattleShips/Assets/Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+	public ParticleSystem smoke;
+	public GameObject exit;
+	// Use this for initialization
+	void Start () {
+		smoke.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		//if ANY cannonball hits the boat, lose one health and destroy the cannonball
+		if (collision.gameObject.tag == "Cannonball") {
+			collision.gameObject.transform.position = exit.transform.position;
+		}
+	}
+}
---
+++ BattleShips/Assets/Scripts/PortalExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalExit : MonoBehaviour {
+	public ParticleSystem smoke;
+	// Use this for initialization
+	void Start () {
+		smoke.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
