b5450fc344cc0914809572d48f4d58b4b08c2444
Sarin Iamsangtham <Iamsangsam@gmail.com>
2018-04-21 22:00:20 -0400

    Started base for Shield Gun of sorts

---
+++ BattleShips/Assets/Scripts/ShieldgunBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldgunBehavior : MonoBehaviour {
+
+    public GameObject sheild;
+    public GameObject LaunchPoint;
+    public AudioSource fireSource;
+
+
+
+    // Use this for initialization
+    void Start () {
+        foreach (Transform child in transform)
+        {
+            if (child.CompareTag("LaunchPoint"))
+            {
+                LaunchPoint = child.gameObject;
+            }
+        }
+
+
+        fireSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Fire()
+    {
+        
+
+        fireSource.Play();
+    }
+}
